f15464b201d0d9f47ffcfe2d146e01dbcc793b87
TRobibinson13 <trerrobinson@gmail.com>
2021-01-02 01:28:01 -0500

    Fixed addition of total and added better console message

--- simplearraysum/src/SimpleArraySum/Program.cs
+++ simplearraysum/src/SimpleArraySum/Program.cs
@@ -10,10 +10,35 @@ class Solution {
      */
     static int simpleArraySum(int[] ar) {
         
-        /*
-         * Write your code here.
-         */
+        int total = 0;
+        int arrayMin = 0;
+        int arrayCount = 0;
 
+        if (ar.Length > arrayMin ){
+
+            foreach(int integer in ar){
+ 
+                if (integer <= 1000){
+                    
+                    total += integer;
+                }
+
+                else{
+
+                    Console.WriteLine($"Integer at array postion of {arrayCount} is out of range with a value of {integer}");
+                    return total = 0;
+                }
+
+                arrayCount ++;
+
+            }
+            return total;
+        }
+
+        else{
+            Console.WriteLine($"Array length out of range with a value of {ar.Length}");
+            return total = 0;
+        }
     }
 
     static void Main(string[] args) {
